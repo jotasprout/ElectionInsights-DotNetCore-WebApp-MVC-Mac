26256d856c667a38eac282dfd7fc0c3bc76b04de
Jay Sprout <jotasprout@gmail.com>
2022-08-29 00:31:38 -0400

    first commit

---
+++ ElectionInsights-DotNetCore-WebApp-MVC-Mac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ElectionInsights_DotNetCore_WebApp_MVC_Mac.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
